054f614f1e0a51ea434cd484f85cfcc9078f777a
sromelus <46148909+sromelus@users.noreply.github.com>
2020-05-20 23:08:13 -0400

    Updates

---
+++ precache-manifest.7ad12bf4e6f5d626304d3add7309db18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd6e9d4b3fe45aa41f0161fb57baa13",
+    "url": "/courses-portal/index.html"
+  },
+  {
+    "revision": "6840b6e284a2abc64be6",
+    "url": "/courses-portal/static/css/main.d4db0edf.chunk.css"
+  },
+  {
+    "revision": "e81582a462249c84573d",
+    "url": "/courses-portal/static/js/2.39d49008.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/courses-portal/static/js/2.39d49008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6840b6e284a2abc64be6",
+    "url": "/courses-portal/static/js/main.63c6e79c.chunk.js"
+  },
+  {
+    "revision": "13fee37d58f4de1eb1f7",
+    "url": "/courses-portal/static/js/runtime-main.0d307a38.js"
+  }
+]);
